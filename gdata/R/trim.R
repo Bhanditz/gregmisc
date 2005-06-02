8e0d778afa7d4fe0eb4dd62dc030b7e0b34c8528
Nitin Jain <nitin.jain@pfizer.com>
2005-06-02 00:00:00 +0000

    version 2.0.8

--- gdata/R/trim.R
+++
@@ -1,8 +0,0 @@
-# $Id: trim.R,v 1.2 2004/09/03 17:27:44 warneg Exp $
-
-trim <- function(s)
-  {
-    s <- sub("^ +","",s)
-    s <- sub(" +$","",s)
-    s
-  }
--- gtools/R/permute.R
+++
@@ -1,3 +0,0 @@
-# $Id: permute.R,v 1.2 2004/09/03 17:27:45 warneg Exp $
-
-permute <- function(x) sample( x, size=length(x), replace=FALSE )
