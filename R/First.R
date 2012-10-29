aa4f26b4c70bd1f86a8994b28de9dacebe1b7a20
Gregory R. Warnes <greg@warnes.net>
2012-10-29 00:00:00 +0000

    version 2.1.2

--- R/First.R
+++ R/First.R
@@ -1,14 +1,11 @@
-# $Id: First.R 1030 2006-11-29 00:20:20Z warnes $
+# $Id: First.R 1492 2011-09-01 21:45:19Z warnes $
 
 .First.lib <- function(libname, pkgname)
 {
-  library(gdata)
-  library(gplots)
-  library(gmodels)
-  library(gtools)
-  warning(paste("",
-          "The `gregmisc' *package* has converted into a *bundle*",
-          "containing four sub-packages: gdata, gtools, gmodels, and gplots.", 
-          "Please load these packages directly.", sep="\n\t"), 
-          call.=FALSE)
+  packageStartupMessage(
+          "All functionality of the `gregmisc' package has been moved",
+          "into the four 'g' packages: gdata, gtools, gmodels, and gplots. ",
+          "This package is retained to make it easy to install and load",
+          "the full set. Please consider loading these packages directly."
+                        )
 }
